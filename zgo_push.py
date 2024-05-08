9d16491c790e803b6d6a7d092a9ef21df19324f3
tuoni <tuonius@outlook.com>
2024-05-09 00:46:05 +0800

    bug fix

--- zgo_push.py
+++ zgo_push.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sys
 import time
 
@@ -22,6 +23,52 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "cookie": "__mmapiwsid=018f00d2-fea1-7853-aeec-74e60ded414b:dfeee073829af5e1928aa8ae6c89c3316bcb2ce2; HBAffiliate=38%3A173917%3A888ac8d450cf5ba9add065da33577705; leftmenu_hide=; SESSID778d=gqh7rhqdnqe0f9f92uclgs8v5o"
 }
+database_file = "sent_guids.db"
+# 初始化数据库连接
+conn = sqlite3.connect(database_file)
+cursor = conn.cursor()
+
+# 创建商品信息表
+cursor.execute('''CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                price TEXT NOT NULL,
+                stock TEXT NOT NULL)''')
+
+# 创建推送记录表
+cursor.execute('''CREATE TABLE IF NOT EXISTS push_records (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                product_id INTEGER NOT NULL,
+                CONSTRAINT fk_product
+                    FOREIGN KEY (product_id)
+                    REFERENCES products (id))''')
+
+
+def insert_product_info(name, price, stock):
+    try:
+        cursor.execute('''INSERT INTO products (name, price, stock) VALUES (?, ?, ?)''', (name, price, stock))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        # 如果产品已存在，则忽略
+        pass
+
+
+def insert_push_record(product_id):
+    cursor.execute('''INSERT INTO push_records (product_id) VALUES (?)''', (product_id,))
+    conn.commit()
+
+
+def get_product_info():
+    cursor.execute('''SELECT id, name, price, stock FROM products''')
+    return cursor.fetchall()
+
+
+def check_stock_change(product_id, new_stock):
+    cursor.execute('''SELECT stock FROM products WHERE id = ?''', (product_id,))
+    old_stock = cursor.fetchone()[0]
+    return old_stock != new_stock
+
+
 if __name__ == '__main__':
     url = "https://clients.zgovps.com/index.php?/cart/special-offer/"
     try:
@@ -38,14 +85,22 @@ if __name__ == '__main__':
             stock = "Out of stock!" if stock_button.text.strip() == "Out of stock!" else "In stock"
             last_li = product.find_all('div', class_='my-3')[0].find_all('li')[-1].text.strip()
             # print("产品名:", product_name)
+            insert_product_info(product_name, price, stock)
             print(f"{last_li} | {price} | {stock}")
-            # print("品名:", last_li)
-            # print("价格:", price)
-            # print("库存:", stock)
-            if stock == "In stock" and price != "$52.00 USD 年付":
-                print("开始抢购吧!\nhttps://clients.zgovps.com/index.php?/cart/special-offer/")
-                push_message("活动开始了", "开始抢购吧!\nhttps://clients.zgovps.com/index.php?/cart/special-offer/")
-            print()
+            product_info = get_product_info()
+            for info in product_info:
+                product_id, name, _, current_stock = info
+                if product_name == name:
+                    if check_stock_change(product_id, stock):
+                        # 检查是否已经成功推送过
+                        # cursor.execute('''SELECT id FROM push_records WHERE product_id = ?''', (product_id,))
+                        # if not cursor.fetchone():
+                        print("开始抢购吧!\nhttps://clients.zgovps.com/index.php?/cart/special-offer/")
+                        push_message("活动开始了",
+                                     "开始抢购吧!\nhttps://clients.zgovps.com/index.php?/cart/special-offer/")
+                        # 记录推送
+                        cursor.execute('''UPDATE products SET stock = ? WHERE id = ?''', (stock, product_id))
+                        conn.commit()
         time.sleep(1)
     except Exception as e:
         push_message("抢购脚本出错了", f"{str(e)}")
